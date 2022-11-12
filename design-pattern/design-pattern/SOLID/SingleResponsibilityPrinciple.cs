75d7c099fd98636c04cb402ecfd709f83a9d7753
Jay Ponkia <ponkiajay19@gmail.com>
2022-11-12 13:16:48 +0530

    Initial commit for SRP
    - Single Responsibility Principle initial commit

---
+++ design-pattern/design-pattern/SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_pattern.SOLID
+{
+    /// <summary>
+    /// Every software module should have only one reason to change
+    /// </summary>
+    internal class SingleResponsibilityPrinciple
+    {
+    }
+}
