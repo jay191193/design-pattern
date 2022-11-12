42ec002bac1000aaccacb7f548ba14b1908a4c9e
Jay Ponkia <ponkiajay19@gmail.com>
2022-11-12 18:35:35 +0530

    Update Program.cs
    Remove unnecessary references

--- design-pattern/design-pattern/Program.cs
+++ design-pattern/design-pattern/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace design_pattern
+﻿namespace design_pattern
 {
     public class Program
     {
