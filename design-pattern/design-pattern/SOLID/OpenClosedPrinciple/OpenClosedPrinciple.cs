6853142645c9913b8c32acb8e705e8da123c286b
Jay Ponkia <ponkiajay19@gmail.com>
2022-11-13 10:09:28 +0530

    Update OpenClosedPrinciple.cs
    - Added code for calculator with respect to Open Closed Principle.

--- design-pattern/design-pattern/SOLID/OpenClosedPrinciple/OpenClosedPrinciple.cs
+++ design-pattern/design-pattern/SOLID/OpenClosedPrinciple/OpenClosedPrinciple.cs
@@ -4,9 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace design_pattern.SOLID
+namespace design_pattern.OpenClosedPrinciple
 {
-    class OpenClosedPrinciple
+    public interface ICalculate
     {
+        void Calculate();
+    }
+
+    public class Summation : ICalculate
+    {
+        private int _firstNum;
+        private int _secondNum;
+        public Summation(int firstNum, int secondNum)
+        {
+            this._firstNum = firstNum;
+            this._secondNum = secondNum;
+        }
+        public void Calculate()
+        {
+            Console.WriteLine(_firstNum + _secondNum);
+        }
+    }
+
+    public class Subtraction : ICalculate
+    {
+        private int _firstNum;
+        private int _secondNum;
+        public Subtraction(int firstNum, int secondNum)
+        {
+            this._firstNum = firstNum;
+            this._secondNum = secondNum;
+        }
+        public void Calculate()
+        {
+            Console.WriteLine(_firstNum - _secondNum);
+        }
+    }
+
+    public class Multiplication : ICalculate
+    {
+        private int _firstNum;
+        private int _secondNum;
+        public Multiplication(int firstNum, int secondNum)
+        {
+            this._firstNum = firstNum;
+            this._secondNum = secondNum;
+        }
+        public void Calculate()
+        {
+            Console.WriteLine(_firstNum * _secondNum);
+        }
+    }
+
+    public class Division : ICalculate
+    {
+        private int _firstNum;
+        private int _secondNum;
+        public Division(int firstNum, int secondNum)
+        {
+            this._firstNum = firstNum;
+            this._secondNum = secondNum;
+        }
+        public void Calculate()
+        {
+            Console.WriteLine(_firstNum / _secondNum);
+        }
+    }
+    public class Calculator
+    {
+        private ICalculate _calculate;
+
+        public Calculator(ICalculate calculate)
+        {
+            this._calculate = calculate;
+        }
+
+        public void Calculate()
+        {
+            this._calculate.Calculate();
+        }
     }
 }
