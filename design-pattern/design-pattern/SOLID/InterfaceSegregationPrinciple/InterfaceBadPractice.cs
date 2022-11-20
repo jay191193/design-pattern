65baccec7732ec5d89eebba405c1433ebb51affc
Jay Ponkia <ponkiajay19@gmail.com>
2022-11-20 10:46:46 +0530

    Added files in csproj
    CSProj changes

---
+++ design-pattern/design-pattern/SOLID/InterfaceSegregationPrinciple/InterfaceBadPractice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_pattern.SOLID.InterfaceSegregationPrinciple
+{
+    public class Document { }
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+    class InterfaceBadPractice
+    {
+    }
+}
